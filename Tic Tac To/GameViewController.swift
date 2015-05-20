debc806064e461452ffd0ed554519c97383b19ae
BenEmdon <benjaminemdon@gmail.com>
2015-05-20 15:34:46 -0400

    Added Tic Tac Toe game: Currently Unfinished
    I have one player working (no ai)

---
+++ Tic Tac To/GameViewController.swift
@@ -0,0 +1,160 @@
+//
+//  GameViewController.swift
+//  Tic Tac To
+//
+//  Created by Benjamin Emdon on 2015-05-20.
+//  Copyright (c) 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    
+    @IBOutlet var ticTacImg1: UIImageView!
+    @IBOutlet var ticTacImg2: UIImageView!
+    @IBOutlet var ticTacImg3: UIImageView!
+    @IBOutlet var ticTacImg4: UIImageView!
+    @IBOutlet var ticTacImg5: UIImageView!
+    @IBOutlet var ticTacImg6: UIImageView!
+    @IBOutlet var ticTacImg7: UIImageView!
+    @IBOutlet var ticTacImg8: UIImageView!
+    @IBOutlet var ticTacImg9: UIImageView!
+    
+    @IBOutlet var ticTacButton1: UIButton!
+    @IBOutlet var ticTacButton2: UIButton!
+    @IBOutlet var ticTacButton3: UIButton!
+    @IBOutlet var ticTacButton4: UIButton!
+    @IBOutlet var ticTacButton5: UIButton!
+    @IBOutlet var ticTacButton6: UIButton!
+    @IBOutlet var ticTacButton7: UIButton!
+    @IBOutlet var ticTacButton8: UIButton!
+    @IBOutlet var ticTacButton9: UIButton!
+    
+    @IBOutlet var resetButton: UIButton!
+    
+    @IBOutlet var userMessage: UILabel!
+    
+    
+    var plays = [Int: Int]()
+    var done = false
+    var aiDeciding = false
+    
+    
+    @IBAction func UIButtonClicked(sender:UIButton) {
+        userMessage.hidden=true
+        if plays[sender.tag]==nil && !aiDeciding && !done {
+            setImageToSpot(sender.tag, player:1)
+        }
+        
+        checkForWin()
+        aiTurn()
+    }
+    
+    
+    
+    func setImageToSpot(spot:Int, player:Int){
+        var playerMark = player == 1 ? "x-icon" : "o-icon"
+        plays[spot] = player
+        switch spot {
+        case 1:
+            ticTacImg1.image = UIImage(named: playerMark)
+        case 2:
+            ticTacImg2.image = UIImage(named: playerMark)
+        case 3:
+            ticTacImg3.image = UIImage(named: playerMark)
+        case 4:
+            ticTacImg4.image = UIImage(named: playerMark)
+        case 5:
+            ticTacImg5.image = UIImage(named: playerMark)
+        case 6:
+            ticTacImg6.image = UIImage(named: playerMark)
+        case 7:
+            ticTacImg7.image = UIImage(named: playerMark)
+        case 8:
+            ticTacImg8.image = UIImage(named: playerMark)
+        case 9:
+            ticTacImg9.image = UIImage(named: playerMark)
+        default:
+            ticTacImg5.image = UIImage(named: playerMark)
+        }
+    }
+    
+    @IBAction func resetButtonClicked(sender:UIButton) {
+        resetButton.hidden = true
+        userMessage.hidden = true
+        done = false
+        reset()
+    }
+    
+    
+    func reset()
+    {
+        plays = [:]
+        ticTacImg1.image = nil
+        ticTacImg2.image = nil
+        ticTacImg3.image = nil
+        ticTacImg4.image = nil
+        ticTacImg5.image = nil
+        ticTacImg6.image = nil
+        ticTacImg7.image = nil
+        ticTacImg8.image = nil
+        ticTacImg9.image = nil
+        
+    }
+    
+    
+    func checkForWin(){
+        var whoWon = ["I":0,"you":1]
+        for(key,value) in whoWon
+        {
+            if  (plays[1] == value && plays[2] == value && plays[3] == value) || //across the top
+                (plays[4] == value && plays[5] == value && plays[6] == value) || //across the middle
+                (plays[7] == value && plays[8] == value && plays[9] == value) || //across the bottom
+                (plays[1] == value && plays[4] == value && plays[7] == value) || //down the left
+                (plays[2] == value && plays[5] == value && plays[8] == value) || //down the middle
+                (plays[3] == value && plays[6] == value && plays[9] == value) || //down the right
+                (plays[1] == value && plays[5] == value && plays[9] == value) || //diag left right
+                (plays[3] == value && plays[5] == value && plays[7] == value)//diag right left
+            {
+                userMessage.text = "Looks like \(key) won!"
+                userMessage.hidden = false
+                resetButton.hidden = false
+                done = true
+            }
+        }
+        
+        
+        
+    }
+    
+    func aiTurn(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+
+
+
+
+
+
