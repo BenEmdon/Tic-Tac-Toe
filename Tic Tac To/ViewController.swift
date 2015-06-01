8bf6c99ec09ef8c744f69c3d88d0270c51733391
BenEmdon <benjaminemdon@gmail.com>
2015-06-01 13:50:27 -0400

    Created Developer Button

--- Tic Tac To/ViewController.swift
+++ Tic Tac To/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var singleplayerButton: DesignableButton!
+    @IBOutlet var multiplayerButton: DesignableButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+            }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    override func viewWillAppear(animated: Bool) {
+        
+        singleplayerButton.animation = "slideRight"
+        singleplayerButton.animate()
+        multiplayerButton.animation  = "slideLeft"
+        multiplayerButton.animate()
 
+    }
 
 }
 
