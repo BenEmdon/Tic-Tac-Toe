c9f18bfec5ef95c17a0d1586598c5bccdd155215
BenEmdon <benjaminemdon@gmail.com>
2015-06-03 13:19:32 -0400

    Finished follow dev button
    -Still need to fix problem when all spots occupied & player wins
    results in a tie

---
+++ Tic Tac To/FollowDevView.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Tic Tac To
+//
+//  Created by Benjamin Emdon on 2015-06-02.
+//  Copyright (c) 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+class FollowDevView: UIViewController
+{
+    @IBAction func followButtonDidPress(sender: AnyObject) {
+        UIApplication.sharedApplication().openURL(NSURL(string:"https://twitter.com/Ben_Emdon")!)
+            self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
